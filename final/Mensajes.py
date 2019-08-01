ef6f3544fdf16f2a7e3ba54faf0cdb3c1a1eb79e
Luis Alberto Castro <luis_bg@outlook.com>
2019-08-01 18:03:59 -0500

    integracion de unittest
    en este se integra la prueba unitaria

--- final/Mensajes.py
+++ final/Mensajes.py
@@ -8,6 +8,7 @@ Created on Thu Aug  1 12:43:13 2019
 import DecimalToHexadecimal
 import BinarioToDecimal
 import Capicua
+import unittest
 class Mensajes:
     def textoAtras(texto):
         textoDecimal=str(texto)
@@ -23,3 +24,20 @@ class Mensajes:
         Capicuas=Capicua.Capicua.NumeroCapicua(texto)
         textoCompleto='Siquiente: ' +textoDecimal+'\nBinario: '+textoBinario+'\nHexadecimal: '+textoHexadecimal+'\nCapicua: '+Capicuas
         return textoCompleto
+class Test(unittest.TestCase):
+ def test_sum(self):
+        valoresperado='atrás: 22\nBinario: 10110\nHexadecimal: 16\nCapicua: el número 22 es capicúa'
+        valorObtenido=Mensajes.textoAtras(22)
+        self.assertEqual(valorObtenido,valoresperado)
+ def test_dos(self):
+        valoresperado= 'atrá[15 chars] 1011010\nHexadecimal: 5a\nCapicua: el número 90 no es capicúa'
+        valorObtenido=Mensajes.textoAtras(90)
+        self.assertEqual(valorObtenido,valoresperado) 
+ def test_tres(self):
+        valoresperado= 'atrás: 80\nBinario: 10110\nHexadecimal: 16\nCapicua: el número 80 no es capicúa'
+        valorObtenido=Mensajes.textoSiguiente(80)
+        self.assertEqual(valorObtenido,valoresperado)       
+ if __name__ == "__main__":
+  unittest.main()       
+     
+             
